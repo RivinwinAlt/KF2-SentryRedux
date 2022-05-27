2d9d0b90e2351979050235e48da3234f01e94f01
RivinwinAlt <92415999+RivinwinAlt@users.noreply.github.com>
2022-05-27 09:30:38 -0400

    Uploaded scripts

---
+++ Scripts/Compiling and Uploading/1a MakeMods.bat
@@ -0,0 +1,3 @@
+set editorExe="G:\SteamSlow\steamapps\common\killingfloor2\Binaries\Win64\KFEditor.exe"
+
+Start "" %editorExe% make -useunpublished -platform=PC -log
---
+++ Scripts/Compiling and Uploading/4 CleanPublishedAndCache.bat
@@ -0,0 +1,9 @@
+REM Clear Brewed packages
+RD /S /Q "C:\Users\joshu\Documents\My Games\KillingFloor2\KFGame\Published\BrewedPC\Packages\SentryRedux"
+
+REM Clear downloaded mods
+RD /S /Q "C:\Users\joshu\Documents\My Games\KillingFloor2\KFGame\Cache"
+
+REM Clear all compiled scripts
+DEL "C:\Users\joshu\Documents\My Games\KillingFloor2\KFGame\Unpublished\BrewedPC\Script\SentryRedux.u"
+DEL "C:\Users\joshu\Documents\My Games\KillingFloor2\KFGame\Published\BrewedPC\SentryRedux.u"
